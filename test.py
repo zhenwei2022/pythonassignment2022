1769d96f9c98656bc2ff3ae1ed53ca5417b3249a
Wei Zhen <zhen.wei@metropola.fi>
2022-08-25 16:09:02 +0300

    # Lab 2.3 import math # num1 = "the length of the rectangle" # num2 = "the width of the rectangle" # perimeter = 2 * (num1 + num2) # area = num1 * num2 num1=3 num2=2 print(2*(num1+num2)) print((num1 * num2))
    # Lab 2.2
    import math
    r = 2
    print((math.pi) * r * r)
    
    # Lab 2.1
    name1 = "Viivi"
    name2 = "Ahmed"
    print("Hello," + name1+ "!")
    print("Hello," + name2+ "!")
    
    # Lab 1.1
    print("Hello, Wei Zhen!")

---
+++ test.py
@@ -0,0 +1,24 @@
+# Lab 2.3
+import math
+# num1 = "the length of the rectangle"
+# num2 = "the width of the rectangle"
+# perimeter = 2 * (num1 + num2)
+# area = num1 * num2
+num1=3
+num2=2
+print(2*(num1+num2))
+print((num1 * num2))
+
+# Lab 2.2
+import math
+r = 2
+print((math.pi) * r * r)
+
+# Lab 2.1
+name1 = "Viivi"
+name2 = "Ahmed"
+print("Hello," + name1+ "!")
+print("Hello," + name2+ "!")
+
+# Lab 1.1
+print("Hello, Wei Zhen!")
