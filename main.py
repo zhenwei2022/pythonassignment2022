1b497519aaa2849bafd6c121042a5740774bea94
Wei Zhen <zhen.wei@metropola.fi>
2022-08-26 11:20:18 +0300

    # Lab 2.6 import random value = random.randint(000,999) print(value) import random value = random.randint(1111,6666) print(value)
    #Lab 2.5
    #num1="enter talents"
    #num2="enter prouds"
    #nub3="enter lots"
    num1=3
    num2=9
    num3=13.5
    print(((num1*20 + num2) * 32 + num3) * 13.3)
    print((str(int(29545.95/1000)) + "kilograms"))
    gram = 29545.95%1000
    print(str(round(gram,2)) +"grams")
    
    #Lab 2.4
    num1 = 2
    num2 = 3
    num3 = 4
    print(num1 + num2 + num3)
    print(num1 * num2 * num3)
    print((num1 + num2 + num3)/3)
    
    # Lab 2.3
    import math
    # num1 = "the length of the rectangle"
    # num2 = "the width of the rectangle"
    # perimeter = 2 * (num1 + num2)
    # area = num1 * num2
    num1=3
    num2=2
    print(2*(num1+num2))
    print((num1 * num2))
    
    # Lab 2.2
    import math
    r = 2
    print((math.pi) * r * r)
    
    # Lab 2.1
    name1 = "Viivi"
    name2 = "Ahmed"
    print("Hello," + name1+ "!")
    print("Hello," + name2+ "!")
    
    # Lab 1.1
    print("Hello, Wei Zhen!")

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# Lab 2.6
+import random
+value = random.randint(000,999)
+print(value)
+import random
+value = random.randint(1111,6666)
+print(value)
+
 #Lab 2.5
 #num1="enter talents"
 #num2="enter prouds"
@@ -7,4 +15,38 @@ num2=9
 num3=13.5
 print(((num1*20 + num2) * 32 + num3) * 13.3)
 print((str(int(29545.95/1000)) + "kilograms"))
-print(str(29545.95 % 1000) + "grams")
+gram = 29545.95%1000
+print(str(round(gram,2)) +"grams")
+
+#Lab 2.4
+num1 = 2
+num2 = 3
+num3 = 4
+print(num1 + num2 + num3)
+print(num1 * num2 * num3)
+print((num1 + num2 + num3)/3)
+
+# Lab 2.3
+import math
+# num1 = "the length of the rectangle"
+# num2 = "the width of the rectangle"
+# perimeter = 2 * (num1 + num2)
+# area = num1 * num2
+num1=3
+num2=2
+print(2*(num1+num2))
+print((num1 * num2))
+
+# Lab 2.2
+import math
+r = 2
+print((math.pi) * r * r)
+
+# Lab 2.1
+name1 = "Viivi"
+name2 = "Ahmed"
+print("Hello," + name1+ "!")
+print("Hello," + name2+ "!")
+
+# Lab 1.1
+print("Hello, Wei Zhen!")
