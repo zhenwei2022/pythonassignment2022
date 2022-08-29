3960e0b4354de8cf9f1b8c950080f96522552081
Wei Zhen <zhen.wei@metropola.fi>
2022-08-29 23:20:51 +0300

    # Lab4.1 num = 1 while 3 * num <= 1000:     print(3 * (num))     num = num + 1
    # Lab4.2
    inch = float(input("enter inch: "))
    while inch >= 0:
        print(str(2.54 * inch) + "cm")
        break
    while inch < 0:
        break

---
+++ Lab4.py
@@ -0,0 +1,16 @@
+# Lab4.1
+num = 1
+while 3 * num <= 1000:
+    print(3 * (num))
+    num = num + 1
+
+# Lab4.2
+inch = float(input("enter inch: "))
+while inch >= 0:
+    print(str(2.54 * inch) + "cm")
+    break
+while inch < 0:
+    break
+
+
+
